86ec3c9d70f9c7e3db6ebbf7a571f397fbc6fa22
Anna Park <parkannuh@gmail.com>
2022-04-28 23:26:37 +0000

    Solved 'add' algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -1,1 +1,7 @@
-console.log('hi');
+// Write a function that returns the sum of two numbers.
+// Input: param1 = 1 and param2 = 2.
+// Output: solution(param1, param2) = 3.
+
+function solution(param1, param2) {
+  return param1 + param2;
+}
