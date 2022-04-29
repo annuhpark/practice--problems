a1b1336dc3cbed0df2263ffed99b3ce6fb174cef
Anna Park <parkannuh@gmail.com>
2022-04-29 00:07:20 +0000

    Solved 'shapeArea' algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -99,3 +99,12 @@ function solution(inputArray) {
   }
   return sample;
 }
+
+// We will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+// A 1 - interesting polygon is just a square with a side of length 1. An n - interesting polygon is obtained by taking the n - 1 - interesting polygon and appending 1 - interesting polygons to its rim, side by side.
+// Input: n = 3.
+// Output: solution(n) = 13.
+
+function solution(n) {
+  return (n * n) + ((n - 1) * (n - 1));
+}
