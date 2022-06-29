1f97982b9b5ea282c51879c03fdcc70e8a4cd5c8
Anna Park <parkannuh@gmail.com>
2022-06-29 05:37:17 +0000

    Solved containsDuplicate algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -315,3 +315,19 @@ function sumTwoSmallestNumbers(numbers) {
   numbers = numbers.sort(function (a, b) { return a - b; });
   return numbers[0] + numbers[1];
 };
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Input: nums = [1, 2, 3, 1]
+// Output: true
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+var containsDuplicate = function (nums) {
+  const unique = [...new Set(nums)];
+  console.log(unique, nums);
+  if (JSON.stringify(unique) === JSON.stringify(nums)) {
+    return false;
+  } else {
+    return true;
+  }
+};
