d7f9d471ccc3ab3fe617129a364b27bd03823e19
Anna Park <parkannuh@gmail.com>
2022-06-29 05:45:56 +0000

    Solved isAnagram algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -331,3 +331,22 @@ var containsDuplicate = function (nums) {
     return true;
   }
 };
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Input: s = "rat", t = "car"
+// Output: false
+
+var isAnagram = function (s, t) {
+  const sortedS = s.split('').sort();
+  const sortedT = t.split('').sort();
+  if (JSON.stringify(sortedS) === JSON.stringify(sortedT)) {
+    return true;
+  } else {
+    return false;
+  }
+};
