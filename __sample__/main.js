4ee87c3b5bb6b20d26867f56a2a2c450326a0c0f
Anna Park <parkannuh@gmail.com>
2022-06-29 19:50:44 +0000

    Solved twoSum algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -350,3 +350,30 @@ var isAnagram = function (s, t) {
     return false;
   }
 };
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1]
+
+// Input: nums = [3, 2, 4], target = 6
+// Output: [1, 2]
+
+// Input: nums = [3, 3], target = 6
+// Output: [0, 1]
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let x = 0; x < nums.length; x++) {
+      if (i === x) {
+        continue;
+      }
+      const remainingNum = target - nums[i];
+      if (nums[x] === remainingNum) {
+        return [i, x];
+      }
+    }
+  }
+}
