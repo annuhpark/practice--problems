53209d75f49f8cf1cf579cd4a939a7cfa619352a
Anna Park <parkannuh@gmail.com>
2022-07-03 00:04:57 +0000

    Solved groupAnagrams algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -377,3 +377,32 @@ function twoSum(nums, target) {
     }
   }
 }
+
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Input: strs = [""]
+// Output: [[""]]
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+var groupAnagrams = function(strs) {
+  const sortedStrs = strs.map(word => word.split('').sort().join(''));
+  // console.log(sortedStrs);
+  const hash = {};
+  for (let i = 0; i < strs.length; i++) {
+    if (!hash[sortedStrs[i]]) {
+      hash[sortedStrs[i]] = [strs[i]];
+    } else {
+      hash[sortedStrs[i]].push(strs[i]);
+    }
+  }
+  return Object.values(hash);
+};
+
+groupAnagrams(["eat","tea","tan","ate","nat","bat"]);
