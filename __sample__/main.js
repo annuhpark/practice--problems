949bae79278af3e7dba321a419a5a750dbe7cd13
Anna Park <parkannuh@gmail.com>
2022-07-05 22:57:49 +0000

    Solved topKFrequent algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -406,3 +406,36 @@ var groupAnagrams = function(strs) {
 };
 
 groupAnagrams(["eat","tea","tan","ate","nat","bat"]);
+
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+var topKFrequent = function(nums, k) {
+  // console.log(nums);
+  const hash = {};
+  const finalArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i]);\
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = [nums[i]];
+    } else {
+      hash[nums[i]].push(nums[i]);
+    }
+  }
+  const groupedElements = Object.values(hash);
+  let topFrequent = [];
+  // console.log(groupedElements);
+  const elementsInOrder = groupedElements.sort(function(a, b) {
+    return b.length - a.length
+  });
+  // console.log(elementsInOrder);
+  for (let i = 0; i < k; i++) {
+    finalArr.push(elementsInOrder[i][0]);
+  }
+  return finalArr;
+};
