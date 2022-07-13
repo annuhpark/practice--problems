c25aed6959925fd9b944841cce1bfbbf746c5131
Anna Park <parkannuh@gmail.com>
2022-07-13 07:50:37 +0000

    Solved Product of Array Except Self algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -462,3 +462,33 @@ var isPalindrome = function(s) {
     return false;
   }
 };
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+
+var productExceptSelf = function(nums) {
+  let finalArray = [];
+  let counter = 0;
+  let finalProduct = 1;
+  for (let i = 0; i < nums.length; i++) {
+    for (let x = 0; x < nums.length; x++) {
+      if (i === x) {
+        continue;
+      } else {
+        finalProduct = finalProduct * nums[x];
+      }
+    }
+    finalArray.push(finalProduct);
+    finalProduct = 1;
+  }
+  return finalArray;
+};
