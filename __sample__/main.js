47a37ed32176cb8eb1e76a1e12c3e887f6872057
Anna Park <parkannuh@gmail.com>
2022-07-26 00:48:54 +0000

    Solved twoSum II algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -351,18 +351,17 @@ var isAnagram = function (s, t) {
   }
 };
 
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-// You can return the answer in any order.
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
-// Input: nums = [2, 7, 11, 15], target = 9
-// Output: [0, 1]
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 
-// Input: nums = [3, 2, 4], target = 6
-// Output: [1, 2]
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
 
-// Input: nums = [3, 3], target = 6
-// Output: [0, 1]
+// Input: numbers = [2,3,4], target = 6
+// Output: [1,3]
+// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
 
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
