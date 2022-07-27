a8ba5c414e6bef7c0021ff6906c19a8f38228712
Anna Park <AnnaPark.Developer@gmail.com>
2022-07-27 21:22:53 +0000

    Solved fizzbuzz algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -491,3 +491,25 @@ var productExceptSelf = function(nums) {
   }
   return finalArray;
 };
+
+// We are going to write a function called fizzbuzz that will accept no arguments.
+
+// The goal of this function is to print out all numbers from 1 to 100 but with three exceptions:
+
+// For every number that is divisible by 3 and 5, console log "FizzBuzz".
+// For every number that is divisible by only 3 and not 5, console log "Fizz".
+// For every number that is divisible by only 5 and not 3, console .log "Buzz".
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0 && i % 5 !== 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
