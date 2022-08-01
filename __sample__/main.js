005ab3b770811a9c83e5091f10073bc21697422b
Anna Park <AnnaPark.Developer@gmail.com>
2022-08-01 22:32:46 +0000

    Solved simpleArraySum algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -520,3 +520,21 @@ function fizzBuzz(n) {
 SELECT COMPANY.ID
   FROM COMPANY
   WHERE COMPANY.EMPLOYEES > 10000;
+
+// Given an array of integers, find the sum of its elements.
+
+// Function Description
+
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+// simpleArraySum has the following parameter(s):
+
+// ar: an array of integers
+
+function simpleArraySum(ar) {
+  let counter = 0;
+  for (let i = 0; i < ar.length; i++) {
+    counter = counter + ar[i];
+  }
+  return counter;
+}
