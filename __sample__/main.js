547320fe828a0d28d21c177b98c95ceae76c8ca9
Anna Park <AnnaPark.Developer@gmail.com>
2022-08-06 05:42:54 +0000

    Solved birthdayCakeCandles algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -607,3 +607,16 @@ function miniMaxSum(arr) {
     let minVal = Math.min(...arr);
     console.log((sum - maxVal) + ' ' + (sum - minVal));
 }
+
+// You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+function birthdayCakeCandles(candles) {
+  let biggestNumber = Math.max(...candles);
+  let counter = 0;
+  for (let i = 0; i < candles.length; i++) {
+    if (candles[i] === biggestNumber) {
+      counter = counter + 1;
+    }
+  }
+  return counter;
+}
