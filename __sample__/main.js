f6b2dbad7e7e3b49fc6ab115869d7f214574b641
Anna Park <AnnaPark.Developer@gmail.com>
2022-09-20 19:11:26 +0000

    Solved buy and sell stock algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -678,3 +678,28 @@ function solution(a) {
   }
   return finalArray;
 }
+
+// Best time to buy and sell stock:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+function maxProfit(prices) {
+  let buy = prices[0];
+  prices[0] = 0;
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (buy > prices[i]) {
+      buy = prices[i];
+      prices[i] = 0;
+    } else {
+      profit = Math.max(prices[i] - buy, profit);
+    }
+    return profit;
+  }
+}
