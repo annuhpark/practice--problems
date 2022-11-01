613e2a03f62f1e617d86045f7d5db1c68d07589c
Anna Park <AnnaPark.Developer@gmail.com>
2022-11-01 19:44:59 +0000

    Solved maxSubArray algo.

--- __sample__/main.js
+++ __sample__/main.js
@@ -707,3 +707,22 @@ function maxProfit(prices) {
     return profit;
   }
 }
+
+// Maximum Subarray: Given an integer array nums, find the subarray which has the largest sum and return its sum.
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+var maxSubArray = function (nums) {
+  let maxCurrent = nums[0];
+  let maxGlobal = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+    if (maxCurrent > maxGlobal) {
+      maxGlobal = maxCurrent;
+    }
+  }
+  return maxGlobal;
+};
